7107bfb12c422a1dc7281e40d8eac5162ad68c5d
QuocDung21 <rongvang2357hn@gmail.com>
2023-02-09 14:40:56 +0700

    update add file env

--- routes/user.js
+++
@@ -1,9 +0,0 @@
-const middlewareController = require('../controllers/middlewareController');
-const userController = require('../controllers/userController');
-const Router = require('express').Router()
-
-Router.post('/register', userController.registerUser)
-Router.post('/login', userController.loginUser)
-Router.get('/', middlewareController.verifyToken, userController.getAllUser)
-Router.post('/refresh', userController.requestRefreshToken)
-module.exports = Router
