f23d17d8ef7348c8d55746680041dda490108aef
QuocDung21 <rongvang2357hn@gmail.com>
2023-04-17 23:17:22 +0700

    success

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
-app.get("/", (req, res) => {
+app.get("/api/run", (req, res) => {
   res.json("Api running...");
 });
 app.use("/api/users", authRouter);
